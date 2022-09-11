5f632224b1c004d6dfd0457474abd460075ef450
Bharathwaj <bharathwajist@gmail.com>
2022-09-12 00:30:50 +0530

    Add files via upload

---
+++ Cryptography/reverseCipher.py
@@ -0,0 +1,10 @@
+message = "Three can keep a scret, if two of them are dead"
+message = "daed era meht fo owt fi ,tercs a peek nac eerhT"
+translated = ''
+
+i = len(message) - 1
+while i>=0:
+    translated = translated + message[i]
+    i = i-1
+
+print(translated)
